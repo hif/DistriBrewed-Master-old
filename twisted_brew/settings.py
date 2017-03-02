f7badb5ff59f7bc15adc87cd15d99822dba01fe6
hif <hif@simnet.is>
2017-03-02 23:13:50 +0000

    Mostly import fixes

--- twisted_brew/settings.py
+++ twisted_brew/settings.py
@@ -19,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'database/twistedbrew.db',               # Or path to database file if using sqlite3.
+        'NAME': '/Users/hif/PycharmProjects/DistriBrewed-Master/database/twistedbrew.db',               # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         #'USER': 'admin',
         #'PASSWORD': 'admin',
